af152d8b693299371f3e0639bbe83c4d930b8746
aashi22-ai <aashlesharani@gmail.com>
2026-05-02 00:23:21 +0530

    fix vite base for vercel

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio-pro/',   // replace with your exact GitHub repo name
+  base: '/',
 })
